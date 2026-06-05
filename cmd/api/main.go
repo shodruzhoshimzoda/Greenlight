32934b7a9638fcc9b905d89a8833c3d1b292488a
sabrrise <shodruzxoshimzoda@gmail.com>
2026-06-05 22:15:52 +0500

    First commit. Was created server with one handler

---
+++ cmd/api/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+// api version
+const version = "1.0.0"
+
+// appilcation configuration
+
+type config struct {
+	port 	int
+	env 	string
+} 
+type application struct {
+	cfg 	config
+	logger  *slog.Logger
+}
+
+
+
+func main() {
+	var cfg config
+	
+	flag.IntVar(&cfg.port, "port", 8080, "API server port" )
+	flag.StringVar(&cfg.env, "env", "development", "Environment development|staging|production")
+	flag.Parse()
+
+
+	// initializing logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+
+	app := &application{
+		cfg: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/v1/healthcheck", app.healthcheck)
+
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%d", cfg.port),
+		Handler: mux,
+		IdleTimeout: time.Minute,
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("starting server on port:", "addr", srv.Addr, "env",cfg.env)
+	err := srv.ListenAndServe()
+
+	logger.Error(err.Error())
+
+	os.Exit(1)
+	
+}
