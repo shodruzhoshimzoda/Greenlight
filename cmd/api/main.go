c11af2d1ef4348ce97171901d2580b2f67ad6b23
sabrrise <shodruzxoshimzoda@gmail.com>
2026-06-09 00:39:26 +0500

    Установил библеотеку httprouter для более удобной работы с сервером и для разработки. Заменил стандартный mux на httprouter

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -42,14 +42,10 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/v1/healthcheck", app.healthcheck)
-
 
 	srv := &http.Server{
 		Addr: fmt.Sprintf(":%d", cfg.port),
-		Handler: mux,
+		Handler: app.routes() ,
 		IdleTimeout: time.Minute,
 		ReadTimeout: 5 * time.Second,
 		WriteTimeout: 10 * time.Second,
