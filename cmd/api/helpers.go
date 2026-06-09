6d46d29e8299f4254c79b6acb548b755568edfba
sabrrise <shodruzxoshimzoda@gmail.com>
2026-06-09 23:50:26 +0500

    Создал функциб для чтения Id от запроса

---
+++ cmd/api/helpers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+// Read id parametr from request
+func (app *application) readIdParams(r *http.Request) (int64, error) {
+
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)  
+
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parametrs")
+	}
+
+	return id, nil
+
+
+	
+}
