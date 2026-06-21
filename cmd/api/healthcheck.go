7b9ad46ab0ba6fd4e055ffa73b9fb32516039669
sabrrise <shodruzxoshimzoda@gmail.com>
2026-06-21 17:37:55 +0500

    Was updated healthcheck method and used json encoder

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 
 
 func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.cfg.env)
-	fmt.Fprintf(w, "version :%s\n", version)
+
+	data := map[string]string{
+		"status":"available",
+		"environment":app.cfg.env,
+		"version":version,
+	}
+
+
+	js, err := json.Marshal(data); 
+	
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+
+
 }
