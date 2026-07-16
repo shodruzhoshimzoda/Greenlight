5e89ca3b13d36dc77875c48e49e06ccf58afdfad
sabrrise <shodruzxoshimzoda@gmail.com>
2026-07-16 20:19:26 +0500

    Customizin Runtime JSON for decoding

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,16 +1,15 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
-
-
-
-
 
 type Runtime int
 
+var InvalidRuntimeFormat = errors.New("invalid runtime format")
 
 // This method make raadable and customizable runtime field. Example: 120 mins
 func (r Runtime) MarshalJSON() ([]byte, error) {
@@ -19,8 +18,26 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 
 	quotedJSONValues := strconv.Quote(jsonValue)
 
-
 	return []byte(quotedJSONValues), nil
 
+}
 
+func (r *Runtime) UnmarshalJSON(b []byte) error {
+	unQuotedJSONValues, err := strconv.Unquote(string(b))
+	if err != nil {
+		return InvalidRuntimeFormat
+	}
+	parts := strings.Split(unQuotedJSONValues, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return InvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return InvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
