3ea4709f437e6ad0741b8844dd891db2e5c72147
sabrrise <shodruzxoshimzoda@gmail.com>
2026-07-19 23:15:37 +0500

    Connection to PostgreSQL

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 // api version
 const version = "1.0.0"
 
-// appilcation configuration
+// application configuration
 
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 type application struct {
 	cfg    config
@@ -28,16 +35,26 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment development|staging|production")
+	// Connection to PostgreSQL DSN
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight", "PostgreSQL DSN")
 	flag.Parse()
 
 	// initializing logger
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error("error opening database", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
 
 	app := &application{
 		cfg:    cfg,
 		logger: logger,
 	}
 
+	logger.Info("Connection to PostgreSQL established")
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
@@ -48,10 +65,31 @@ func main() {
 	}
 
 	logger.Info("starting server on port:", "addr", srv.Addr, "env", cfg.env)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 
 	logger.Error(err.Error())
 
 	os.Exit(1)
 
+}
+
+// Function for connection to PostgreSQL
+func openDB(cfg config) (*sql.DB, error) {
+
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close() // close connection with Database
+		return nil, err
+	}
+
+	return db, nil
 }
